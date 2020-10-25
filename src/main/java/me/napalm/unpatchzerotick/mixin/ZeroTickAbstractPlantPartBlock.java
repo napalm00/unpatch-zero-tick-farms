cba8da47dd832b2fd44ab65fc30eb71bcab8471b
naPalm <odnapalm@gmail.com>
2020-10-25 12:15:54 +0100

    Updated for 1.16.3 Fixed mod not working for plants (kelp, vines...)

--- src/main/java/me/napalm/unpatchzerotick/mixin/ZeroTickAbstractPlantPartBlock.java
+++ src/main/java/me/napalm/unpatchzerotick/mixin/ZeroTickAbstractPlantPartBlock.java
@@ -15,27 +15,53 @@
 
 package me.napalm.unpatchzerotick.mixin;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.SugarCaneBlock;
+import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Random;
 
-@Mixin(SugarCaneBlock.class)
-public class SugarCaneBlockTickMixin {
-	@Shadow
-	public void randomTick(final BlockState state, final ServerWorld world, final BlockPos pos, final Random random) {}
+@Mixin(AbstractPlantPartBlock.class)
+public class ZeroTickAbstractPlantPartBlock  {
 
-	@Inject(at = @At("TAIL"), method = "scheduledTick")
-	public void scheduledTick(final BlockState state, final ServerWorld world, final BlockPos pos, final Random random, CallbackInfo info) {
-		if(!world.isAir(pos.down())) {
-			this.randomTick(state, world, pos, random);
-		}
-	}
+    @Inject(at = @At("TAIL"), method = "scheduledTick")
+    public void scheduledTick(final BlockState state, final ServerWorld world, final BlockPos pos, final Random random, CallbackInfo info) {
+
+        if (!state.canPlaceAt(world, pos)) {
+            world.breakBlock(pos, true);
+            return;
+        }
+
+        Block curBlock = state.getBlock();
+        BlockPos curPos = pos;
+
+        // if weeping vines, find stem by going down
+        if(curBlock.toString().contains("weeping")) {
+            while (curBlock.toString().contains("_plant")) {
+                curPos = curPos.down();
+                curBlock = world.getBlockState(curPos).getBlock();
+            }
+        }
+        else {
+            // otherwise find stem by going up
+            while (curBlock.toString().contains("_plant")) {
+                curPos = curPos.up();
+                curBlock = world.getBlockState(curPos).getBlock();
+            }
+        }
+
+        if(!world.isAir(curPos)) {
+            // once stem is found, call randomTick on it
+            try {
+                AbstractPlantStemBlock stemBlock = AbstractPlantStemBlock.class.cast(curBlock);
+                stemBlock.randomTick(world.getBlockState(curPos), world, curPos, random);
+            } catch (Exception ex) {
+            }
+        }
+    }
 }
+
